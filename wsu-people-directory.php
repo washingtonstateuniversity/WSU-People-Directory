47f79910b8a01fec07dfe565c1ab400e85819f34
jeremyfelt <jeremy.felt@gmail.com>
2015-07-16 11:50:00 -0700

    Bump version 0.1.2

--- wsu-people-directory.php
+++ wsu-people-directory.php
@@ -4,7 +4,7 @@ Plugin Name: WSU People Directory
 Plugin URI:	https://web.wsu.edu/wordpress/plugins/wsu-people-directory/
 Description: A plugin to maintain a central directory of people.
 Author:	washingtonstateuniversity, CAHNRS, philcable, danialbleile, jeremyfelt
-Version: 0.1.1
+Version: 0.1.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -17,7 +17,7 @@ class WSUWP_People_Directory {
 	 *
 	 * @var string
 	 */
-	var $personnel_plugin_version = '0.1.1';
+	var $personnel_plugin_version = '0.1.2';
 
 	/**
 	 * The slug used to register the "Personnel" custom content type.
