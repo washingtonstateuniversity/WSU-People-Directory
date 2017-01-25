c7924e6e3363f554cc1151e51806d7af90aa332b
Phil Cable <pcable@wsu.edu>
2017-01-25 11:54:00 -0800

    Add plugin skeleton structure

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function( grunt ) {
+    grunt.initConfig( {
+        pkg: grunt.file.readJSON( "package.json" ),
+
+        phpcs: {
+            plugin: {
+                src: [ "./*.php", "./includes/*.php" ]
+            },
+            options: {
+                bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
+                standard: "phpcs.ruleset.xml"
+            }
+        },
+
+        jscs: {
+            scripts: {
+                src: [ "Gruntfile.js", "js/*.js" ],
+                options: {
+                    preset: "jquery",
+                    requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
+                    maximumLineLength: 250
+                }
+            }
+        },
+
+        jshint: {
+            grunt_script: {
+                src: [ "Gruntfile.js" ],
+                options: {
+                    curly: true,
+                    eqeqeq: true,
+                    noarg: true,
+                    quotmark: "double",
+                    undef: true,
+                    unused: false,
+                    node: true     // Define globals available when running in Node.
+                }
+            },
+           people_scripts: {
+                src: [ "js/*.js" ],
+                options: {
+                    bitwise: true,
+                    curly: true,
+                    eqeqeq: true,
+                    forin: true,
+                    freeze: true,
+                    noarg: true,
+                    nonbsp: true,
+                    quotmark: "double",
+                    undef: true,
+                    unused: true,
+                    browser: true, // Define globals exposed by modern browsers.
+                    jquery: true   // Define globals exposed by jQuery.
+                }
+            }
+        }
+    } );
+
+	grunt.loadNpmTasks( "grunt-jscs" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-phpcs" );
+
+    // Default task(s).
+    grunt.registerTask( "default", [ "phpcs", "jscs", "jshint" ] );
+};
---
+++ tests/test-wsu-people-directory.php
@@ -0,0 +1,10 @@
+<?php
+
+class WSUWP_People_Directory_Tests extends WP_UnitTestCase {
+	/**
+	 * An initial sample test to verify working tests.
+	 */
+	public function test_WSUWP_People_Directory_function() {
+		$this->assertTrue( true );
+	}
+}
