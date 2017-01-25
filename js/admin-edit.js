7d153b1f1ef1edd849ab113ceba34c59dc9ab448
Phil Cable <pcable@wsu.edu>
2017-01-25 15:30:11 -0800

    Ajust JavaScript files for coding standards and JSHint errors

--- js/admin-edit.js
+++ js/admin-edit.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function( $ ) {
+( function( $, document ) {
+	$( document ).ready( function() {
 
 		// Maybe heavy-handed, but these labels have no unique attributes to leverage.
-		$( '.inline-edit-categories-label:contains("Appointments")' ).remove();
-		//$( '.inline-edit-categories-label:contains("Classifications")' ).remove();
-
-});
+		$( ".inline-edit-categories-label:contains('Appointments')" ).remove();
+	} );
+}( jQuery, document ) );
