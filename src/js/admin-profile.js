4ec576dae32f06c8d31df924f4c9128603dfa46a
Phil Cable <pcable@wsu.edu>
2017-02-15 11:02:24 -0800

    Add JavaScript for image uploading/selecting

--- src/js/admin-profile.js
+++ src/js/admin-profile.js
@@ -1,3 +1,4 @@
+/* global _ */
 ( function( $, window, document ) {
 	$( document ).ready( function() {
 
@@ -43,7 +44,7 @@
 
 			var data = {
 				"action": "wsu_people_get_data_by_nid",
-				"_ajax_nonce": window.wsupeople_nid_nonce,
+				"_ajax_nonce": window.wsupeople.nid_nonce,
 				"network_id": $( "#_wsuwp_profile_ad_nid" ).val()
 			};
 
@@ -91,6 +92,174 @@
 				}
 			} );
 
+		} );
+
+		// Photo collection handling.
+		var media_frame,
+			details_frame,
+			$add_photo = $( ".wsuwp-profile-add-photo" ),
+			$collection = $( ".wsuwp-profile-photo-collection" ),
+			$count = $( ".wsuwp-profile-photo-count" ),
+			$tooltip = $( ".wsuwp-profile-photo-controls-tooltip" );
+
+		// Handle photo adding.
+		$add_photo.on( "click", function( e ) {
+			e.preventDefault();
+
+			if ( media_frame ) {
+				media_frame.open();
+				return;
+			}
+
+			media_frame = window.wp.media( {
+				title: "Select or Upload Your Photos",
+				multiple: true,
+				library: {
+					type: "image",
+					uploadedTo: window.wsupeople.post_id
+				},
+				button: {
+					text: "Use photo(s)"
+				}
+			} );
+
+			media_frame.on( "select", function() {
+				var photos = media_frame.state().get( "selection" ),
+					template = _.template( $( "#photo-template" ).html() ),
+					existing_photos = [];
+
+				// Create an array of photo IDs already in the collection.
+				$( ".wsuwp-profile-photo-id" ).each( function() {
+					existing_photos.push( parseInt( $( this ).val() ) );
+				} );
+
+				$.each( photos.models, function( i, attachment ) {
+
+					var index = Number( $count.val() ) + i,
+						photo = attachment.toJSON(),
+						has_thumbnail = photo.sizes.hasOwnProperty( "thumbnail" ),
+						url = has_thumbnail ? photo.sizes.thumbnail.url : photo.url,
+						width = has_thumbnail ? photo.sizes.thumbnail.width : photo.width,
+						height = has_thumbnail ? photo.sizes.thumbnail.height : photo.height;
+
+					// An image doesn't need to be added more than once.
+					if ( -1 === $.inArray( photo.id, existing_photos ) ) {
+						$collection.append( template( {
+							src: url,
+							width: width,
+							height: height,
+							count: index,
+							id: photo.id,
+							alt: photo.alt,
+							url: photo.url,
+							title: photo.title,
+							full_width: photo.width,
+							full_height: photo.height
+						} ) );
+					}
+				} );
+
+				$count.val( $collection.find( ".wsuwp-profile-photo-wrapper" ).length );
+			} );
+
+			media_frame.open();
+		} );
+
+		// Surface control buttons when an image is clicked.
+		$( "html" ).click( function( e ) {
+			if ( $( e.target ).hasClass( "wsuwp-profile-photo" ) ) {
+				$( e.target ).next( ".wsuwp-profile-photo-controls" ).show();
+				$( e.target ).closest( ".wsuwp-profile-photo-wrapper" ).siblings( ".wsuwp-profile-photo-wrapper" ).find( ".wsuwp-profile-photo-controls" ).hide();
+			} else {
+				$( ".wsuwp-profile-photo-controls" ).hide();
+			}
+		} );
+
+		// Show control buttons tooltip.
+		$collection.on( "mouseover", ".wsuwp-profile-photo-controls button", function() {
+			var text = this.getAttribute( "aria-label" ),
+				button = this.getBoundingClientRect(),
+				collection = $collection[ 0 ].getBoundingClientRect();
+
+			$tooltip.find( ".wsuwp-profile-photo-controls-tooltip-inner" ).html( text );
+
+			$tooltip.css( {
+				top: button.bottom - collection.top + "px",
+				left: button.right - collection.left - $tooltip.width() / 2 - 6 + "px"
+			} ).show();
+		} );
+
+		// Hide control buttons tooltip.
+		$collection.on( "mouseleave", ".wsuwp-profile-photo-controls button", function() {
+			$tooltip.hide();
+		} );
+
+		// Edit a photo.
+		$collection.on( "click", ".wsuwp-profile-photo-edit", function( e ) {
+			e.preventDefault();
+			$tooltip.hide();
+
+			var img = $( this ).closest( ".wsuwp-profile-photo-wrapper" ).find( "img" ),
+				metadata = {
+				attachment_id: img.data( "id" ),
+				size: "full",
+				caption: "",
+				align: "none",
+				extraClasses: "",
+				link: false,
+				title: img.attr( "title" ),
+				url: img.data( "url" ),
+				alt: img.attr( "alt" ),
+				width: img.data( "width" ),
+				height: img.data( "height" )
+			};
+
+			if ( details_frame ) {
+				details_frame.open();
+				return;
+			}
+
+			window.wp.media.events.trigger( "editor:image-edit", {
+				metadata: metadata,
+				image: img
+			} );
+
+			details_frame = window.wp.media( {
+				frame: "image",
+				state: "image-details",
+				metadata: metadata
+			} );
+
+			details_frame.on( "close", function() {
+				var values = details_frame.state().image.attributes;
+
+				img.attr( "title", values.title );
+				img.attr( "alt", values.alt );
+				img.data( "width", values.width );
+				img.data( "height", values.height );
+
+				// @todo update image post meta to reflect changes
+			} );
+
+			details_frame.open();
+		} );
+
+		// Delete a photo.
+		$collection.on( "click", ".wsuwp-profile-photo-remove", function( e ) {
+			e.preventDefault();
+			$tooltip.hide();
+
+			$( this ).closest( ".wsuwp-profile-photo-wrapper" ).remove();
+
+			var $remaining_photos = $( ".wsuwp-profile-photo-id" );
+
+			// Reset indexes of the remaining photos in the collection.
+			for ( var i = 0; i < $remaining_photos.length; i++ ) {
+				$( $remaining_photos[ i ] ).attr( "name", "photos[" + i + "]" );
+			}
+
+			// Reset count value.
+			$count.val( $remaining_photos.length );
 		} );
 	} );
 }( jQuery, window, document ) );
