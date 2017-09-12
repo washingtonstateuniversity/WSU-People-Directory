5a5bb4ff86d195887a3f7f2153323dc8568c3070
Phil Cable <pcable@wsu.edu>
2017-09-12 09:30:05 -0700

    Bump version 0.3.7

--- includes/class-wsuwp-people-directory.php
+++ includes/class-wsuwp-people-directory.php
@@ -14,7 +14,7 @@ class WSUWP_People_Directory {
 	 *
 	 * @var string
 	 */
-	public static $version = '0.3.6';
+	public static $version = '0.3.7';
 
 	/**
 	 * Maintain and return the one instance. Initiate hooks when called the first time.
